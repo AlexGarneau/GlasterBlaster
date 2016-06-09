ea189dd1f8a9de63b9582c1e81bc2e013a85c3e3
Alex <mechasonic@msn.com>
2016-06-08 20:00:30 -0600

    Canvas draws nothing, but it's something.
    Signed-off-by: Alex <mechasonic@msn.com>

--- src/GlasterBlaster.js
+++ src/GlasterBlaster.js
@@ -10,6 +10,14 @@
 
     p.init = function () {
         this.canvas = document.getElementById("glasterblaster");
+        this.stage = new createjs.Stage(this.canvas);
+
+        var graphics = new createjs.Graphics();
+        graphics.f("#0040DF").dr(0, 0, 800, 600);
+        var shape = new createjs.Shape(graphics);
+        this.stage.addChild(shape);
+
+        this.stage.update();
     };
 
     scope.GlasterBlaster = GlasterBlaster;
